c0c4106d7f6505d922fc847cdb03999bcde674cf
pyrotek45 <pyrotek45_gaming@yahoo.com>
2021-10-29 19:01:10 -0400

    using TStringlist for the source code

--- main_modul.pas
+++ main_modul.pas
@@ -1,86 +1,73 @@
 program Main_model;
 {$mode objfpc}{$H+}
 uses
-    sysutils,
-    strutils,
-    fgl,
-    classes,
-    crt,
-    modul_interpreter;
+  sysutils,
+  strutils,
+  fgl,
+  classes,
+  crt,
+  modul_interpreter;
 
 procedure repl;
 var
-    user : string;
-    mim : mim_interpreter;
+  user : string;
+  mim : mim_interpreter;
+
 begin
-    mim := mim_interpreter.create;
+  mim := mim_interpreter.create;
 
-    writeln('type exit to close the program');
-    writeln('type reset to clear mim');
-    repeat 
-        write('>> ');
-        readln(user); 
+  writeln('type exit to close the program');
+  writeln('type reset to clear mim');
 
-        case (user) of
-            //'help': {*show help screen*};
-            'reset' : 
-                begin
-                    mim.free;
-                    mim := mim_interpreter.create;
-                end;
+  repeat
+    write('>> ');
+    readln(user);
+
+    case (user) of
+      //'help': {*show help screen*};
+      'reset' :
+        begin
+          mim.free;
+          mim := mim_interpreter.create;
         end;
-
-        if not (user = '') then begin
-            mim.parse(lowercase(user));
-        end;
-
-    until (lowercase(user) = 'exit');
-end;
-
-procedure SaveString(InString, OutFilePath: string);
-var
-    F: TextFile;
-begin
-    AssignFile(F, OutFilePath);
-    try
-        ReWrite(F);
-        Write(F, InString);
-    finally
-    CloseFile(F);
     end;
+
+    if not (user = '') then begin
+        mim.parse(lowercase(user));
+    end;
+
+  until (lowercase(user) = 'exit');
 end;
 
 var
-    modul_file,setup_file : textfile;
-    command,input,filename: string;
-    I : integer;
-    mim : mim_interpreter;
+  mim : mim_interpreter;
+  source : TStringlist;
+  line : string;
+
 begin
 
-    if paramstr(1) = '' then
-    begin
-        //writeln('no file: use mim <filename>');
-        repl;
-        exit;
-    end;
+  //Testing for command line parameter
+  if paramstr(1) = '' then
+   begin
+      //writeln('no file: use mim <filename>');
+      repl;
+      exit;
+  end;
 
-    mim := mim_interpreter.create;
+  //Creating the interpreter
+  mim := mim_interpreter.create;
 
-    Assignfile(setup_file, paramstr(1));
-    reset(setup_file);
+  //Loading the source code
+  source := TStringlist.create();
+  source.LoadFromFile(paramstr(1));
 
-    while not eof(setup_file) do begin
-        readln(setup_file,command); 
-        if not (command = '') then begin
-            //writeln('FULL COMMAND: ',lowercase(command));
-            mim.parse(lowercase(command));
-            //mim.get_full_info;
-            //SaveString(mim.variable_str,'mim_log.mh')
-        end;
-    end;
+  //Send each line into the interpreter
+  for line in source do
+  begin
+    mim.parse(lowercase(line));
+  end;
 
-    //writeln(mim.get_memory: 0: 2);
+  //Free the interpreter from memory
+  mim.destroy;
 
-    CloseFile(setup_file);
-    mim.destroy;
 end.
